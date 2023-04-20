ca8152beae999566b2c8faa656ccec92679577f6
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-04-20 17:52:23 +0800

    if &, change to e

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # import pyfiglet module
 import pyfiglet
 
+# change font color
+class ANSI():
+    def color_text(code):
+        return "\33[{code}]m".format(code=code)
+
 # ask user for input then save it
 input_str = input("What is your input string? ")
 output_str = ""
@@ -12,7 +17,7 @@ for i in range(len(input_str)):
         output_str += "a"
 # if &, change to e
     elif input_str[i] == "&":
-        output_str += "a"
+        output_str += "e"
 # if #, change to i
     elif input_str[i] == "#":
         output_str += "i"
@@ -24,6 +29,11 @@ for i in range(len(input_str)):
         output_str += "u"
     else:
         output_str += input_str[i]
+
+# print output
+print(pyfiglet.figlet_format(output_str, font = "bubble"))
+output_str = ANSI.color_text(96) + output_str
+print(output_str)
 
 
 
