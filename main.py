18ce15137c62e2e6a6f60a33afdc43c1f26b4d9e
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-04-20 17:43:17 +0800

    if &, change to e

--- main.py
+++ main.py
@@ -11,15 +11,21 @@ for i in range(len(input_str)):
     if input_str[i] == "*":
         output_str += "a"
 # if &, change to e
-    if input_str[i] == "&":
+    elif input_str[i] == "&":
         output_str += "a"
 # if #, change to i
-    if input_str[i] == "#":
+    elif input_str[i] == "#":
         output_str += "i"
 # if +, change to o
-    if input_str[i] == "+":
+    elif input_str[i] == "+":
         output_str += "o"
-        
+# if !, change to u
+    elif input_str[i] == "!":
+        output_str += "u"
+    else:
+        output_str += input_str[i]
+
+
 
 
 
